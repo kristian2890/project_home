870addd69bfafeec5dcc327c0b8936fd33df8912
Kristian Buhl Jørgensen <kristian2890@gmail.com>
2025-07-09 12:18:23 +0000

    Add and commit changes

--- src/portfolio_site/settings.py
+++ src/portfolio_site/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY') if DEBUG else {{ project.DJANGO_SECRET_KEY }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=Csv()) if DEBUG else {{ project.VADJANGO_ALLOWED_HOSTSRIABLENAME }}
 
 
 # Application definition
